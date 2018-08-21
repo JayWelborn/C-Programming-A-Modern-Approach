4bbd5513037e793ab4932f04efb56d9de8c515a7
jaywelborn <jesse.welborn@gmail.com>
2018-08-21 12:36:33 +0200

    Chapter 5 exercise 2

---
+++ Chapter05/exercise02.c
@@ -0,0 +1,24 @@
+//
+// Created by jwelborn on 8/21/18.
+//
+#include <stdio.h>
+
+/**
+ * Program to convert 24-hour time to 12-hour time
+ */
+ int main(void) {
+     int hours, minutes;
+     char suffix[2] = {'A', 'M'};
+     printf("Enter a 24-hour time: ");
+     scanf("%d:%d", &hours, &minutes);
+     if (hours > 12) {
+         hours %= 12;
+     }
+     if (hours >= 12) {
+         suffix[0] = 'P';
+     }
+     if (hours == 0) {
+         hours = 12;
+     }
+     printf("Equivalent 12-hour time: %02d:%02d %s", hours, minutes, suffix);
+ }
