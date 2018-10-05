a3c65042b75569ba256e4503b155bde5c8529148
jaywelborn <jesse.welborn@gmail.com>
2018-10-05 20:45:28 +0200

    Chapter 13 Exercise 18

---
+++ Chapter13/exercise18.c
@@ -0,0 +1,22 @@
+//
+// Created by jwelborn on 10/5/18.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main( void )
+{
+    char *months[] = {
+            "January", "February", "March",
+            "April", "May", "June", "July",
+            "August", "September", "October",
+            "November", "December"
+    };
+
+    int month, day, year;
+    printf("Enter a date (mm/dd/yyyy): ");
+    scanf("%d/%d/%d", &month, &day, &year);
+
+    printf("You entered the date: %s %d, %d\n", months[month - 1], day, year);
+}
